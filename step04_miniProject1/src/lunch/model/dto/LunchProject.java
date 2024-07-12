edfecc5455634303c7219a5a5915bfcc587ca400
yuwan kang <kangyuwan@naver.com>
2024-07-12 15:29:33 +0900

    lunch 프로젝트

--- step04_miniProject1/src/lunch/model/dto/LunchProject.java
+++ step04_miniProject1/src/lunch/model/dto/LunchProject.java
@@ -9,10 +9,9 @@ import lombok.Setter;
 @Getter
 public class LunchProject {
 	/** 프로젝트 고유 이름 : lunch- 프로젝트 구분 데이터 */
-	private String lunchProjectName;
+	private String food;
 	/** 팀  : 팀번호 , 팀원명, 팀원명 정보 */
 	private Team projectTeam;
-		
 	/** 음식 */
 	private Food projectFood;
 	/** 점심 타입 */
@@ -22,25 +21,25 @@ public class LunchProject {
 	/** 프로젝트 종료일 */
 	private String endDate;
 	/** 진행되는 project의 상세 내용 */
-	private String talentDonationProjectDetail;
+	private String Detail;
 	
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
-		builder.append("프로젝트명 : ");
-		builder.append(talentDonationProjectName);
-		builder.append(", 기부자 : ");
-		builder.append(projectDonator);
-		builder.append(", 수혜자 : ");
-		builder.append(projectBeneficiary);
-		builder.append(", 진행되는 기부 타입 : ");
-		builder.append(talentDonationType);
-		builder.append(", 기부 시작일 : ");
+		builder.append("음식종류 : ");
+		builder.append(food);
+		builder.append(", 팀 : ");
+		builder.append(projectTeam);
+		builder.append(", 음식 : ");
+		builder.append(projectFood);
+		builder.append(", 음식 종류 : ");
+		builder.append(LunchType);
+		builder.append(", 점심시간 시작 : ");
 		builder.append(startDate);
-		builder.append(", 기부 종료일 : ");
+		builder.append(", 점심시간 종료 : ");
 		builder.append(endDate);
-		builder.append(", 기부 상세 내용 : ");
-		builder.append(talentDonationProjectDetail);
+		builder.append(", 기타 내용 : ");
+		builder.append(Detail);
 		return builder.toString();
 	}
 }
