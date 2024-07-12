0b9351783ee461c8e7e6c2b5e2d1201dc5542898
yuwan kang <kangyuwan@naver.com>
2024-07-12 11:23:31 +0900

    hello world

--- step04_miniProject/src/lunch/view/EndView.java
+++ step04_miniProject/src/lunch/view/EndView.java
@@ -1,5 +1,7 @@
 package lunch.view;
 
+
 public class EndView {
+	
 
 }
--- step04_miniProject/src/lunch/view/StartView.java
+++ step04_miniProject/src/lunch/view/StartView.java
@@ -1,5 +1,8 @@
 package lunch.view;
 
 public class StartView {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
 
 }
